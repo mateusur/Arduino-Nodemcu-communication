9c953a8a055c3ace3f15de90ad0f67e7a7fb2121
mateusur <34159544+mateusur@users.noreply.github.com>
2019-07-12 12:42:23 +0200

    Add files via upload

---
+++ round2SlaveARDUINO.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+
+void setup() {
+ Wire.begin(8);                /* join i2c bus with address 8 */
+ Wire.onReceive(receiveEvent); /* register receive event */
+ //Wire.onRequest(requestEvent); /* register request event */
+ Serial.begin(9600);           /* start serial for debug */
+ pinMode(13, OUTPUT);
+ digitalWrite(13, LOW);
+}
+
+void loop() {
+//  while (0 <Wire.available()) {
+//    char c = Wire.read();      /* receive byte as a character */
+//    Serial.print(c);/* print the character */
+//    if(c =="A")
+//    {
+//     digitalWrite(13, LOW);   // turn the LED on (HIGH is the voltage level)
+//     delay(3000);     
+//    }
+//  }
+//         /* to newline */
+}
+
+// function that executes whenever data is received from master
+void receiveEvent(int howMany) {
+ while (0 <Wire.available()) {
+    int c = Wire.read();      /* receive byte as a character */
+    Serial.print(c);/* print the character */
+    if(c==65)
+    {
+     digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    }
+    else if(c==60)
+    {
+    digitalWrite(13,LOW);
+    }
+  }
+ Serial.println();             /* to newline */
+}
+
+//// function that executes whenever data is requested from master
+//void requestEvent() {
+// Wire.write("Hello NodeMCU");  /*send string on request */
+//}
